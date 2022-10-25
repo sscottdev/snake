cc0517b76dc95e121a69b9f954626c21e2f1200d
Sean <sean@sscottdev.com>
2022-10-25 13:11:47 -0400

    added fps counter

--- index.js
+++ index.js
@@ -31,18 +31,35 @@ function setSpeed() {
 
 }
 
+var fps = document.getElementById("fps");
+var startTime = Date.now();
+var frame = 0;
+
+function tick() {
+    var time = Date.now();
+    frame++;
+    if (time - startTime > 1000) {
+        fps.innerHTML = (frame / ((time - startTime) / 1000)).toFixed(1);
+        startTime = time;
+        frame = 0;
+    }
+    window.requestAnimationFrame(tick);
+}
+tick();
+
+
 
 
 function enableDevTools() {
     document.getElementById("myBtn").style.display = "block";
     devWarning.style.display = "none";
     localStorage.clear()
+    fps.style.display = "block";
     highscoreView.innerHTML = "Highscore: " + localStorage.getItem("highscore")
 }
 
 function disableDevTools(){
     devWarning.style.display = "none";
-    devWarning.style.display = "block";
 }
 
 
